92cacb1a02c6b9ae3cca8a6a4736d29b67e2ccd7
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-06-06 22:37:54 +0200

    Some cleaning

--- MpiCreator.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by marek on 06.06.16.
-//
-
-#include "MpiCreator.h"
--- main_mpi.cpp
+++ main_mpi.cpp
@@ -12,39 +12,26 @@ const int numberOfHouses = 10;
 const int numberOfFences = 5;
 MPI_Datatype mpi_message_type;
 
-void enterHouseQueue()
-{
-
-}
+MPI_Datatype initMpiStruct();
+void enterHouseQueue();
 
 int main (int argc, char* argv[])
 {
+    // starting
     int rank, size;
-
     MPI_Init (&argc, &argv);      /* starts MPI */
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);        /* get current process id */
     MPI_Comm_size (MPI_COMM_WORLD, &size);        /* get number of processes */
-
-    const int numberOfItems = 3;
-    int blockLengths[3] = {1,1,1};
-    MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_INT};
-    MPI_Aint offsets[3];
-
-    offsets[0] = offsetof(Message, clock);
-    offsets[1] = offsetof(Message, processId);
-    offsets[2] = offsetof(Message, requestType);
-
-    MPI_Type_create_struct(numberOfItems, blockLengths, offsets, types, &mpi_message_type);
-    MPI_Type_commit(&mpi_message_type);
-
     printf( "Hello world from process %d of %d\n", rank, size );
-    
+    mpi_message_type = initMpiStruct();
+
+    //declaration private data
     std::vector<bool> houses(numberOfHouses, true);
     int fences = numberOfFences;
     auto clock = LamportClock();
 
+    // serving
     MPI_Status status;
-
     Message msg;
     if(rank==0)
     {
@@ -62,4 +49,27 @@ int main (int argc, char* argv[])
 
     MPI_Finalize();
     return 0;
+}
+
+
+MPI_Datatype initMpiStruct()
+{
+    const int numberOfItems = 3;
+    int blockLengths[3] = {1,1,1};
+    MPI_Datatype mpi_message_type;
+    MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_INT};
+    MPI_Aint offsets[3];
+
+    offsets[0] = offsetof(Message, clock);
+    offsets[1] = offsetof(Message, processId);
+    offsets[2] = offsetof(Message, requestType);
+
+    MPI_Type_create_struct(numberOfItems, blockLengths, offsets, types, &mpi_message_type);
+    MPI_Type_commit(&mpi_message_type);
+    return mpi_message_type;
+}
+
+void enterHouseQueue()
+{
+
 }
